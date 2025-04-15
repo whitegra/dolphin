d90b1c2f6d64dd75f8de630d4df21b741f671305
Gracie White <139240618+whitegra@users.noreply.github.com>
2025-04-15 14:39:53 -0400

    Update __init__.py

--- dolphin/__init__.py
+++ dolphin/__init__.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
 from .tensor import Tensor
-from .layers import Linear
+from .layers import Linear, LayerNorm
 from .activations import softmax, gelu
-from .loss import cross_entropy_loss
-from .optimizers import SGD, Adam
+from .loss import cross_entropy_loss, binary_cross_entropy_loss
+from .optimizers import SGD, Adam, Momentum
 from .transformers import TransformerEncoderBlock, MultiHeadSelfAttention, FeedForward
 
 # Expose the main components
-__all__ = ["Tensor", "Linear", "relu", "softmax", "gelu", "cross_entropy_loss", "SGD", "Adam", "TransformerEncoderBlock", "MultiHeadSelfAttention", "FeedForward"]
-
+__all__ = ["Tensor", "Linear", "LayerNorm", "relu", "softmax", "gelu", "cross_entropy_loss", "binary_cross_entropy_loss", "SGD", "Adam", "Momentum", "TransformerEncoderBlock", "MultiHeadSelfAttention", "FeedForward"]
